a512374f70bf71d59ee2f5344d173c93a96d3ccc
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-03-01 00:36:19 +0900

    refactor: DB와의 일관성을 유지하기 위해 컬럼 명 명시

--- src/main/java/com/chukchuk/haksa/domain/BaseEntity.java
+++ src/main/java/com/chukchuk/haksa/domain/BaseEntity.java
@@ -16,9 +16,10 @@ import java.time.Instant;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", updatable = false)
     private Instant createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     private Instant updatedAt;
 }
