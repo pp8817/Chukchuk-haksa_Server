294ea3cb1993de17c5ace29ee681152149e37847
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-02-28 19:07:22 +0900

    Project initial setup

---
+++ src/main/java/com/chukchuk/haksa/ChukchukHaksaApplication.java
@@ -0,0 +1,13 @@
+package com.chukchuk.haksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukchukHaksaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChukchukHaksaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chukchuk/haksa/ChukchukHaksaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chukchuk.haksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChukchukHaksaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
