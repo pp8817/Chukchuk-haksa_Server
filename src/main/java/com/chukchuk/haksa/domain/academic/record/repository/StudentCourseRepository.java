df5c325bae033de901a8a70d0a3ddd639b4d1fe4
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-03-05 22:33:56 +0900

    feat: 수강 과목 조회 메서드 구현 - StudentCourse Service, Dto, Repository 개발

---
+++ src/main/java/com/chukchuk/haksa/domain/academic/record/repository/StudentCourseRepository.java
@@ -0,0 +1,31 @@
+package com.chukchuk.haksa.domain.academic.record.repository;
+
+import com.chukchuk.haksa.domain.academic.record.model.StudentCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+    @Query("""
+        SELECT sc FROM StudentCourse sc
+        JOIN FETCH sc.offering co
+        JOIN FETCH co.course c
+        join FETCH sc.student st
+        LEFT JOIN FETCH co.professor p
+        WHERE st.id = :studentId
+        AND co.year = :year
+        AND co.semester = :semester
+    """)
+    List<StudentCourse> findByStudentIdAndYearAndSemester(
+            @Param("studentId") UUID studentId,
+            @Param("year") Integer year,
+            @Param("semester") Integer semester
+    );
+}
+
+
---
+++ src/main/java/com/chukchuk/haksa/domain/academic/record/service/StudentCourseService.java
@@ -0,0 +1,25 @@
+package com.chukchuk.haksa.domain.academic.record.service;
+
+import com.chukchuk.haksa.domain.academic.record.model.StudentCourse;
+import com.chukchuk.haksa.domain.academic.record.repository.StudentCourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static com.chukchuk.haksa.domain.academic.record.dto.StudentCourseDto.CourseDetailDto;
+
+@Service
+@RequiredArgsConstructor
+public class StudentCourseService {
+    private StudentCourseRepository studentCourseRepository;
+
+    public List<CourseDetailDto> getStudentCourses(UUID studentId, Integer year, Integer semester) {
+        List<StudentCourse> courses = studentCourseRepository.findByStudentIdAndYearAndSemester(studentId, year, semester);
+
+        return courses.stream().map(CourseDetailDto::from)
+                .collect(Collectors.toList());
+    }
+}
