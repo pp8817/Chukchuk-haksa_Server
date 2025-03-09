dd4fe67337925ab61e82332188401cc9db8587f6
0-Jhin <yyoungjin1229@gmail.com>
2025-03-09 18:30:08 +0900

    feat: 예외 처리 코드 작성(#15) -신입생 예외 처리 FreshManException 추가 -@RestControllerAdvice, @ExceptionHandler를 활용한 예외 처리 코드 추가

---
+++ src/main/java/com/chukchuk/haksa/global/exception/FreshManException.java
@@ -0,0 +1,7 @@
+package com.chukchuk.haksa.global.exception;
+
+public class FreshManException extends RuntimeException {
+    public FreshManException(String message) {
+        super(message);
+    }
+}
