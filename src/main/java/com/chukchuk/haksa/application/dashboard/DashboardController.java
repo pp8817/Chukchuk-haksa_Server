0bcd7784c817d5320bee8f18b6ce6fb172e691e2
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-03-26 14:45:33 +0900

    feat: Dashboard API Swagger 적용

--- src/main/java/com/chukchuk/haksa/application/dashboard/DashboardController.java
+++ src/main/java/com/chukchuk/haksa/application/dashboard/DashboardController.java
@@ -1,6 +1,8 @@
 package com.chukchuk.haksa.application.dashboard;
 
 import com.chukchuk.haksa.application.dashboard.dto.DashboardResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/dashboard")
 @RequiredArgsConstructor
+@Tag(name = "Dashboard", description = "사용자 대시보드 관련 API")
 public class DashboardController {
 
     private final DashboardService dashboardService;
 
     /* 사용자 대시보드 정보 조회 API */
     @GetMapping
+    @Operation(summary = "사용자 대시보드 정보 조회", description = "로그인된 사용자의 대시보드 정보를 조회합니다.")
     public ResponseEntity<DashboardResponse> getDashboard(
             @AuthenticationPrincipal UserDetails userDetails
             ) {
