aa32fc80b8caf81db81187775d95056de61b55b7
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-02 16:11:34 +0900

    feat: 회원가입 중복 검사 로직 개발 및 코드 분리 - email 중복 검사

--- src/main/java/com/chukchuk/haksa/domain/user/service/UserService.java
+++ src/main/java/com/chukchuk/haksa/domain/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.chukchuk.haksa.domain.user.dto.UserDto;
 import com.chukchuk.haksa.domain.user.model.User;
 import com.chukchuk.haksa.domain.user.repository.UserRepository;
 import com.chukchuk.haksa.global.exception.DataNotFoundException;
+import com.chukchuk.haksa.global.exception.DuplicationException;
 import com.chukchuk.haksa.global.security.service.JwtProvider;
 import com.chukchuk.haksa.global.security.service.KakaoOidcService;
 import io.jsonwebtoken.Claims;
@@ -34,42 +35,55 @@ public class UserService {
                 .orElseThrow(() -> new DataNotFoundException("User not found"));
     }
 
-    public UUID getUserId(String email) {
-        return userRepository.findByEmail(email)
-                .map(user -> user.getId())
-                .orElseThrow(() -> new DataNotFoundException("User not found"));
+    @Transactional
+    public UserDto.SignInResponse signInWithKakao(UserDto.SignInRequest signInRequest) throws Exception {
+        Claims claims = verifyKakaoToken(signInRequest);
+        String email = extractEmail(claims);
+
+        validateDuplicateEmail(email);
+
+        User user = findOrCreateUser(email);
+        return generateSignInResponse(user);
     }
 
     @Transactional
-    public UserDto.SignInResponse signInWithKakao(UserDto.SignInRequest signInRequest) throws Exception {
-        // Kakaoㅌ 토큰 검증
-        String idToken = signInRequest.id_token();
-        String nonce = signInRequest.nonce();
-        Claims claims = kakaoOidcService.verifyIdToken(idToken, nonce);
+    public void deleteUserByEmail(UUID userId){
+        userRepository.deleteById(userId);
+        // User - Student 연관관계를 엮어 자동 삭제 로직을 만들어야 하지만, Student 저장 로직이 별도로 존재하지 않아, 연관관계를 형성하지 못함. 수정하기 전까지 임의 삭제 방식으로 진행
+        studentRepository.deleteById(userId);
+    }
 
-        // 클레임에서 필요한 정보 추출
-        String email = claims.get("email", String.class);
+    /* private method */
+    private Claims verifyKakaoToken(UserDto.SignInRequest request) throws Exception {
+        return kakaoOidcService.verifyIdToken(request.id_token(), request.nonce());
+    }
 
-        // User 조회 or 생성
-        User user = userRepository.findByEmail(email)
-                .orElseGet(() -> userRepository.save(User.builder()
-                        .email(email)
-                        .profileNickname("Unknown User")
-                        .build()
+    private String extractEmail(Claims claims) {
+        return claims.get("email", String.class);
+    }
+
+    private void validateDuplicateEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new DuplicationException("이미 존재하는 이메일입니다.");
+        }
+    }
+
+    private User findOrCreateUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseGet(() -> userRepository.save(
+                        User.builder()
+                                .email(email)
+                                .profileNickname("Unknown User")
+                                .build()
                 ));
+    }
 
-        // token 발급
+    private UserDto.SignInResponse generateSignInResponse(User user) {
         String userId = user.getId().toString();
         String accessToken = jwtProvider.createAccessToken(userId, user.getEmail(), "USER");
         AuthDto.RefreshTokenWithExpiry refresh = jwtProvider.createRefreshToken(userId);
         refreshTokenService.save(userId, refresh.token(), refresh.expiry());
 
         return new UserDto.SignInResponse(HttpStatus.OK, accessToken, refresh.token());
-    }
-    @Transactional
-    public void deleteUserByEmail(UUID userId){
-        userRepository.deleteById(userId);
-        // User - Student 연관관계를 엮어 자동 삭제 로직을 만들어야 하지만, Student 저장 로직이 별도로 존재하지 않아, 연관관계를 형성하지 못함. 수정하기 전까지 임의 삭제 방식으로 진행
-        studentRepository.deleteById(userId);
     }
 }
