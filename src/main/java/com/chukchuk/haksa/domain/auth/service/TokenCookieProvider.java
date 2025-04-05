42bc1d012a4ef341c39eef4a6f3bae4aa6d92bcb
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-05 17:10:44 +0900

    feat: 회원가입 스웨거 설명 추가, 로컬 테스트용 secure 설정 변경

--- src/main/java/com/chukchuk/haksa/domain/auth/service/TokenCookieProvider.java
+++ src/main/java/com/chukchuk/haksa/domain/auth/service/TokenCookieProvider.java
@@ -18,7 +18,7 @@ public class TokenCookieProvider {
     public ResponseCookie createAccessTokenCookie(String accessToken) {
         return ResponseCookie.from("accessToken", accessToken)
                 .httpOnly(true)
-                .secure(true)
+                .secure(false) // local 테스트용으로 false 설정
                 .path("/")
                 .maxAge(accessTokenExpiration / 1000)
                 .sameSite("None") // 추후 도메인 일치시켜 변경 Strict로 변경 필요
@@ -28,7 +28,7 @@ public class TokenCookieProvider {
     public ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
                 .httpOnly(true)
-                .secure(true)
+                .secure(false) // local 테스트용으로 false 설정
                 .path("/")
                 .maxAge(refreshTokenExpiration / 1000)
                 .sameSite("None") // 추후 도메인 일치시켜 변경 Strict로 변경 필요
