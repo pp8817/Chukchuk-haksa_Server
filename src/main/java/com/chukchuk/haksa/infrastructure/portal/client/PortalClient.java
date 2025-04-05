afdbc426347366168e8214072703c7cf33ec9444
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-06 03:02:22 +0900

    fix: HttpStatusCode는 int라 열거형으로 매핑

--- src/main/java/com/chukchuk/haksa/infrastructure/portal/client/PortalClient.java
+++ src/main/java/com/chukchuk/haksa/infrastructure/portal/client/PortalClient.java
@@ -5,12 +5,10 @@ import com.chukchuk.haksa.infrastructure.portal.exception.PortalLoginException;
 import com.chukchuk.haksa.infrastructure.portal.exception.PortalScrapeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
-import static org.springframework.http.HttpStatus.LOCKED;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Component
 @RequiredArgsConstructor
@@ -30,7 +28,9 @@ public class PortalClient {
                     .toBodilessEntity()
                     .block();
         } catch (WebClientResponseException e) {
-            String message = switch (e.getStatusCode()) {
+            HttpStatus status = HttpStatus.resolve(e.getStatusCode().value());
+
+            String message = switch (status) {
                 case UNAUTHORIZED -> "아이디나 비밀번호가 일치하지 않습니다.";
                 case LOCKED -> "계정이 잠겼습니다. 포털에서 비밀번호 재발급이 필요합니다.";
                 default -> "로그인 중 오류가 발생했습니다.";
@@ -49,7 +49,8 @@ public class PortalClient {
                     .bodyToMono(RawPortalData.class)
                     .block();
         } catch (WebClientResponseException e) {
-            String message = switch (e.getStatusCode()) {
+            HttpStatus status = HttpStatus.resolve(e.getStatusCode().value());
+            String message = switch (status) {
                 case UNAUTHORIZED -> "아이디나 비밀번호가 일치하지 않습니다.";
                 case LOCKED -> "계정이 잠겼습니다. 포털에서 비밀번호 재발급이 필요합니다.";
                 default -> "크롤링 중 오류가 발생했습니다.";
