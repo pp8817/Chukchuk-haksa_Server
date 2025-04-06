77ac01d0abbbb1bd9a8f5c5a96248803456599c3
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-06 16:56:18 +0900

    refactor: dev 환경과 prod 환경 분리 - dev 환경에서는 쿠키 httpOnly 설정 false

--- src/main/java/com/chukchuk/haksa/domain/auth/service/TokenCookieProvider.java
+++ src/main/java/com/chukchuk/haksa/domain/auth/service/TokenCookieProvider.java
@@ -15,23 +15,30 @@ public class TokenCookieProvider {
     @Value("${security.jwt.refresh-expiration}")
     private long refreshTokenExpiration;
 
+    @Value("${security.cookie.dev-mode:false}")
+    private boolean devMode; // 로컬 프론트와 배포 백엔드 조합에서 true로 설정
+
     public ResponseCookie createAccessTokenCookie(String accessToken) {
         return ResponseCookie.from("accessToken", accessToken)
-                .httpOnly(true)
-                .secure(false) // local 테스트용으로 false 설정
+                .httpOnly(httpOnly())
+                .secure(true)
                 .path("/")
                 .maxAge(accessTokenExpiration / 1000)
-                .sameSite("Lax") // local 테스트용으로 Lax
+                .sameSite("None")
                 .build();
     }
 
     public ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
-                .httpOnly(true)
-                .secure(false) // local 테스트용으로 false 설정
+                .httpOnly(httpOnly())
+                .secure(true)
                 .path("/")
                 .maxAge(refreshTokenExpiration / 1000)
-                .sameSite("Lax") // local 테스트용으로 Lax
+                .sameSite("None")
                 .build();
+    }
+
+    private boolean httpOnly() {
+        return !devMode;
     }
 }
