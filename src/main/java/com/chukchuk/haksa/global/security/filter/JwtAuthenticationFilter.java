d71bdf48bdd1335ed4b5ee1a08271f0c94e39ad4
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-08 02:35:24 +0900

    refactor: JWT 전달 방식 Request Header -> Cookie, jar file update

--- src/main/java/com/chukchuk/haksa/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/chukchuk/haksa/global/security/filter/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import com.chukchuk.haksa.global.security.service.JwtProvider;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -28,14 +29,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        String token = extractTokenFromCookie(request);
+        if (token == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-
-        String token = authHeader.substring(7);
         try {
             Claims claims = jwtProvider.parseToken(token);
             String userId = claims.getSubject();
@@ -47,10 +46,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authToken);
 
-            filterChain.doFilter(request, response);
         } catch (Exception e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Invaild Token");
             return;
         }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) return null;
+
+        for (Cookie cookie : request.getCookies()) {
+            if ("accessToken".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
     }
 }
