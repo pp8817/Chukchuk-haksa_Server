7b34e7907fc8cca731b74ca98309bb885c6f29ad
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-29 13:24:06 +0900

    refactor: Portal Exception ErrorCode 기반으로 리팩토링

--- src/main/java/com/chukchuk/haksa/infrastructure/portal/exception/PortalLoginException.java
+++ src/main/java/com/chukchuk/haksa/infrastructure/portal/exception/PortalLoginException.java
@@ -1,7 +1,19 @@
 package com.chukchuk.haksa.infrastructure.portal.exception;
 
+import com.chukchuk.haksa.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
 public class PortalLoginException extends RuntimeException {
-    public PortalLoginException(String message, Throwable cause) {
-        super(message, cause);
+    private final ErrorCode errorCode;
+
+    public PortalLoginException(ErrorCode errorCode) {
+        super(errorCode.message());
+        this.errorCode = errorCode;
+    }
+
+    public PortalLoginException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.message(), cause);
+        this.errorCode = errorCode;
     }
 }
--- src/main/java/com/chukchuk/haksa/infrastructure/portal/exception/PortalScrapeException.java
+++ src/main/java/com/chukchuk/haksa/infrastructure/portal/exception/PortalScrapeException.java
@@ -1,7 +1,19 @@
 package com.chukchuk.haksa.infrastructure.portal.exception;
 
+import com.chukchuk.haksa.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
 public class PortalScrapeException extends RuntimeException {
-    public PortalScrapeException(String message, Throwable cause) {
-        super(message, cause);
+    private final ErrorCode errorCode;
+
+    public PortalScrapeException(ErrorCode errorCode) {
+        super(errorCode.message());
+        this.errorCode = errorCode;
+    }
+
+    public PortalScrapeException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.message(), cause);
+        this.errorCode = errorCode;
     }
 }
