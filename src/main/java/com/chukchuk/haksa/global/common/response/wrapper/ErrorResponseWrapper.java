09413eb11a1ef071a47e9f6995d1cda476214894
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-29 14:17:37 +0900

    refactor: Swagger 예외 Wrapper 클래스 기본 생성자로 변경

--- src/main/java/com/chukchuk/haksa/global/common/response/wrapper/ErrorResponseWrapper.java
+++ src/main/java/com/chukchuk/haksa/global/common/response/wrapper/ErrorResponseWrapper.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(name = "ErrorResponseWrapper", description = "API 에러 응답 포맷")
 public class ErrorResponseWrapper extends ApiResponse<ErrorResponse> {
 
-    protected ErrorResponseWrapper(boolean success, ErrorResponse data, String message, ErrorDetail error) {
-        super(success, data, message, error);
+    public ErrorResponseWrapper() {
+        super(false, null, null, new ErrorDetail("ERROR_CODE", "에러 메시지", null));
     }
 }
