12168cbbb0d19ece1fe4f1ecc88e323967910975
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-04-29 14:30:00 +0900

    refactor: DTO 이름을 명확하게 변경

--- src/main/java/com/chukchuk/haksa/application/api/dto/PortalLoginResponse.java
+++ src/main/java/com/chukchuk/haksa/application/api/dto/PortalLoginResponse.java
@@ -2,12 +2,12 @@ package com.chukchuk.haksa.application.api.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(name = "PortalLoginMessageResponse", description = "포털 로그인 메시지 응답")
-public class PortalLoginMessageResponse {
+@Schema(name = "PortalLoginResponse", description = "포털 로그인 응답")
+public class PortalLoginResponse {
     @Schema(description = "결과 메시지", example = "로그인 성공")
     String message;
 
-    public PortalLoginMessageResponse(String message) {
+    public PortalLoginResponse(String message) {
         this.message = message;
     }
 }
--- src/main/java/com/chukchuk/haksa/application/api/wrapper/PortalLoginApiResponse.java
+++ src/main/java/com/chukchuk/haksa/application/api/wrapper/PortalLoginApiResponse.java
@@ -1,12 +1,12 @@
 package com.chukchuk.haksa.application.api.wrapper;
 
-import com.chukchuk.haksa.application.api.dto.PortalLoginMessageResponse;
+import com.chukchuk.haksa.application.api.dto.PortalLoginResponse;
 import com.chukchuk.haksa.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "PortalLoginApiResponse", description = "포털 로그인 응답")
-public class PortalLoginApiResponse extends ApiResponse<PortalLoginMessageResponse> {
+public class PortalLoginApiResponse extends ApiResponse<PortalLoginResponse> {
     public PortalLoginApiResponse() {
-        super(true, new PortalLoginMessageResponse("로그인 성공"), null, null);
+        super(true, new PortalLoginResponse("로그인 성공"), null, null);
     }
 }
