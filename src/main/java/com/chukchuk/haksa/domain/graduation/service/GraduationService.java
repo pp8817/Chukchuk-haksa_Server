e9e26f89ec98187b1a35aa69f9f1108a15fde7d1
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-05-26 01:15:23 +0900

    feat: 사용자 졸업 요건 조회(`/api/graduation/progress`) API에 캐싱 전략 적용

--- src/main/java/com/chukchuk/haksa/domain/graduation/service/GraduationService.java
+++ src/main/java/com/chukchuk/haksa/domain/graduation/service/GraduationService.java
@@ -5,6 +5,7 @@ import com.chukchuk.haksa.domain.graduation.dto.GraduationProgressResponse;
 import com.chukchuk.haksa.domain.graduation.repository.GraduationQueryRepository;
 import com.chukchuk.haksa.domain.student.model.Student;
 import com.chukchuk.haksa.domain.student.service.StudentService;
+import com.chukchuk.haksa.infrastructure.redis.RedisCacheStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,17 +20,26 @@ public class GraduationService {
 
     private final StudentService studentService;
     private final GraduationQueryRepository graduationQueryRepository;
+    private final RedisCacheStore redisCacheStore;
 
     /* 졸업 요건 진행 상황 조회 */
     public GraduationProgressResponse getGraduationProgress(UUID studentId) {
-        Student student = studentService.getStudentById(studentId);
+        String key = "student:" + studentId + "graduation-progress";
 
+        GraduationProgressResponse cached = redisCacheStore.get(key, GraduationProgressResponse.class);
+        if (cached != null) {
+            return cached;
+        }
+
+        Student student = studentService.getStudentById(studentId);
         // 전공 코드가 없는 학과도 있으므로 majorId가 없으면 departmentId를 사용
         Long effectiveDepartmentId = student.getMajor() != null ? student.getMajor().getId() : student.getDepartment().getId();
 
         // 졸업 요건 충족 여부 조회
         List<AreaProgressDto> areaProgress = graduationQueryRepository.getStudentAreaProgress(studentId, effectiveDepartmentId, student.getAcademicInfo().getAdmissionYear());
 
-        return new GraduationProgressResponse(areaProgress);
+        GraduationProgressResponse response = new GraduationProgressResponse(areaProgress);
+        redisCacheStore.set(key, response);
+        return response;
     }
 }
